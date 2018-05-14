a247633c7892a50565d04bc5c91eca732a025496
Josiah Green <JosiahGreen90@gmail.com>
2018-05-14 16:01:40 -0700

    All tests pass

--- src/__test__/account-route.test.js
+++ src/__test__/account-route.test.js
@@ -57,4 +57,36 @@ describe('AUTH Router', () => {
         });
     });
   }); 
+  describe('GET from /login', () => {
+    test('GET - 200 success', () => {
+      return pCreateAccountMock()
+        .then((mock) => {
+          return superagent.get(`${apiUrl}/login`)
+            .auth(mock.request.username, mock.request.password);
+        })
+        .then((response) => {
+          expect(response.status).toEqual(200);
+          expect(response.body.token).toBeTruthy();
+        });
+    });
+ 
+    test('GET - 400 for bad request', () => {
+      return pCreateAccountMock()
+        .then((mock) => {
+          return superagent.get(`${apiUrl}/login`)
+            .auth('bad request', mock.request.password);
+        })
+        .catch((response) => {
+          expect(response.status).toEqual(400);
+          expect(response.body).toBeFalsy();
+        });
+    });
+    test('GET /login should respond with 404 status code', () => {
+      return superagent.get(`${apiUrl}/login/puppy`)
+        .then(Promise.reject)
+        .catch((response) => {
+          expect(response.status).toEqual(404);
+        });
+    });
+  });
 });
