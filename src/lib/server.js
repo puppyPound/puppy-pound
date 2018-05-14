8c9bff2ffc6c9517bbd62ac477b526aa3305414b
Mario Flores Jr <m.flores4821@gmail.com>
2018-05-14 10:13:11 -0700

    initial scaffolding

---
+++ src/lib/server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import express from 'express';
+import mongoose from 'mongoose';
+import logger from './logger';
+import authRoutes from '../route/auth-router';
+import profileRoutes from '../route/profile-route';
+import imageRoutes from '../route/image-router';
+import loggerMiddleware from './logger-middleware';
+import errorMiddleware from './error-middleware';
+
+const app = express();
+let server = null;
+
+app.use(loggerMiddleware);
+app.use(authRoutes);
+app.use(profileRoutes);
+app.use(imageRoutes);
+
+app.all('*', (request, response) => {
+  logger.log(logger.INFO, 'Returning a 404 from the catch-all/default route');
+  return response.sendStatus(404);
+});
+
+app.use(errorMiddleware);
+
+const startServer = () => {
+  return mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+      server = app.listen(process.env.PORT, () => {
+        logger.log(logger.INFO, `Server is listening on port ${process.env.PORT}`);
+      });
+    });
+};
+
+const stopServer = () => {
+  return mongoose.disconnect()
+    .then(() => {
+      server.close(() => {
+        logger.log(logger.INFO, 'Server is off');
+      });
+    });
+};
+
+export { startServer, stopServer };
