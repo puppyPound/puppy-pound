13ab91da0e3977c7b986a7cbde31aa1e9790cfe5
Kris Sakarias <ksakarias@gmail.com>
2018-05-15 18:33:13 -0700

    env variables moved

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -2,8 +2,4 @@ process.env.NODE_ENV = 'development';
 process.env.PORT = 7000;
 process.env.MONGODB_URI = 'mongodb://localhost/testing';
 process.env.PUPPY_SECRET = 'llsjdlgjsldkgjslkdjgfskdfgjlsdgfj';
-process.env.TWILIO_ACCOUNT_SID = 'ACae132408438f2006dc424b1df531343d';
-process.env.TWILIO_AUTH_TOKEN = 'cccfedb9e77e52f420c0954bb55753f9';
-process.env.TWILIO_NUMBER = '+12062039420';
-process.env.MY_NUMBER = '+18136792983';
  
